7d3a8e02a8e1fbf9363ba10dbed7ffb525a1b82f
superyaooo <superyaooo@gmail.com>
2019-09-13 15:44:05 -0500

    linq intro

---
+++ Features/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var developers = new Employee[]
+            {
+                new Employee{Id = 1, Name = "bob"},
+                new Employee{Id = 2,Name = "pop"} 
+            };
+
+            var sales = new List<Employee>
+            {
+                new Employee{Id = 3, Name = "doug"}
+            };
+            
+            Console.WriteLine(sales.Count);
+        }
+    }
+}
